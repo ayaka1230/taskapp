faebc07ea413142126d54ae83c9901b77ff19d78
ayaka.nagasaka <free.life.0v0@gmail.com>
2021-04-28 20:59:32 +0900

    基本機能を実装（課題の前まで）

---
+++ taskapp/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 長坂絢加 on 2021/04/28.
+//
+
+import Foundation
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // 日時
+    @objc dynamic var date = Date()
+    
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
